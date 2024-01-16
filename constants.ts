2e182c35f10c220bfed87f522ddde8f78705c901
Folka2134 <folka2134@gmail.com>
2024-01-16 14:33:47 +0000

    created default values

---
+++ constants.ts
@@ -0,0 +1,51 @@
+export const navItems = [
+  {
+    label: "Home",
+    path: "/"
+  },
+  {
+    label: "Courses",
+    path: "/courses"
+  },
+  {
+    label: "Contact",
+    path: "/contact"
+  }
+]
+
+export const courseDefaultValues = {
+  title: "",
+  desc: "",
+  imageUrl: "",
+  price: "",
+  duration: "",
+  datePosted: new Date(),
+  isFree: false
+}
+
+export const dummyCourses = [
+  {
+    id: "1",
+    title: "Test title",
+    desc: "Test description",
+    image: "test image",
+    price: "test price",
+    duration: "test duration"
+  },
+  {
+    id: "2",
+    title: "Test title2",
+    desc: "Test description2",
+    image: "test image2",
+    price: "test price2",
+    duration: "test duration2"
+  },
+  {
+    id: "3",
+    title: "Test title3",
+    desc: "Test description3",
+    image: "test image3",
+    price: "test price3",
+    duration: "test duration3"
+  }
+]
