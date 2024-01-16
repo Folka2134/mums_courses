9f1c2e63bb4f52cb51b66eef62d38d2e25c2aee6
Folka2134 <folka2134@gmail.com>
2024-01-16 14:36:24 +0000

    created course schema

---
+++ lib/database/models/course.model.ts
@@ -0,0 +1,25 @@
+import { Schema, model, models } from "mongoose";
+
+export interface ICourse extends Document {
+  _id: string
+  title: string
+  description: string
+  imageUrl: string
+  price: string
+  duration: string
+  isFree: boolean
+  datePosted: Date
+}
+
+const CourseSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  description: { type: String, required: true, unique: true },
+  imageUrl: { type: String, required: true, unique: true },
+  price: { type: String, required: true },
+  duration: {type: String, required: true },
+  isFree: { type: Boolean, required: true },
+  datePosted: { type: Date, default: Date.now }
+})
+
+const Course = model('Course', CourseSchema) || models.Course
+export default Course;
