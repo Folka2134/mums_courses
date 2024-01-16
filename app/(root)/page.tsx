81cc40f41e05a5a42d1ccb426839361b28b8a598
Folka2134 <folka2134@gmail.com>
2024-01-16 14:34:01 +0000

    displaying dummy data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
-import Navbar from "@/components/shared/Navbar";
-import Image from "next/image";
+import CourseList from "@/components/shared/CourseList";
+import { Button } from "@/components/ui/button";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
+  const user = await getUserById(userId);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-10 p-5">
+      {user.admin && (
+        <div>
+          <h1>Admin function</h1>
+          <Link href="/courses/create">
+            <Button>Create listing</Button>
+          </Link>
+        </div>
+      )}
       <h1>Testing mongodb + clerk shared user data</h1>
+      <CourseList />
     </main>
   );
 }
